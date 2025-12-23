3e81151697ca8ecefc3d43043a923e80a403148b
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-12-23 11:40:20 +0000

    Cleaner scene handling.

--- program/source/main.cpp
+++ program/source/main.cpp
@@ -25,17 +25,15 @@ int try_main(int argc, char *argv[])
 
   game->add_scene<csg::custom_scene>(
     "scene",
-    [](std::shared_ptr<csg::custom_scene> scene)
+    [](const std::shared_ptr<csg::custom_scene> scene)
     {
       scene->set_camera<csg::custom_camera>({{0.0f, 0.0f, 80.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, 1.0f, 0.0f}});
       scene->add_object<csg::player>("player", {{0, 0, 0}, {0, 0, 0}, {1, 1, 1}});
       scene->add_object<csg::environment>("floor", {{0, -61, 0}, {0, 0, 0}, {1, 1, 1}}, csg::floor_texture);
       scene->add_object<csg::environment>("shop", {{80, 25, 0}, {0, 0, 0}, {1, 1, 1}}, csg::shop_texture);
-      scene->add_object<csg::environment>("background1", {{0, 80, -5}, {0, 0, 0}, {1, 1, 1}}, csg::background1_texture);
-      scene->add_object<csg::environment>("background2", {{0, 80, -10}, {0, 0, 0}, {1, 1, 1}},
-                                          csg::background2_texture);
-      scene->add_object<csg::environment>("background3", {{0, 80, -15}, {0, 0, 0}, {1, 1, 1}},
-                                          csg::background3_texture);
+      scene->add_object<csg::environment>("background1", {{0, 80, -3}, {0, 0, 0}, {1, 1, 1}}, csg::background1_texture);
+      scene->add_object<csg::environment>("background2", {{0, 80, -6}, {0, 0, 0}, {1, 1, 1}}, csg::background2_texture);
+      scene->add_object<csg::environment>("background3", {{0, 80, -9}, {0, 0, 0}, {1, 1, 1}}, csg::background3_texture);
     });
   game->set_current_scene("scene");
 
