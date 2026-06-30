4603c9efb7fb6f6ac60ab870b27fc6e52e288146
Connor Sweeney <connor.sweeney1234@gmail.com>
2026-06-30 18:58:12 +0100

    Update to csb 2.3.2.

--- program/include/state.hpp
+++ program/include/state.hpp
@@ -12,20 +12,16 @@ namespace csg
   class settings final : public cse::state
   {
   private:
-#define GAME(X)                                                                                                        \
-  X(master, double, {0.5})                                                                                             \
-  X(sound, double, {1.0})                                                                                              \
-  X(music, double, {1.0})
-    ENLIST(game, GAME);
-#undef GAME
-#define WINDOW(X)                                                                                                      \
-  X(display, SDL_DisplayID, {PRIMARY})                                                                                 \
-  X(position, (std::pair<int, int>), {ORIGIN, ORIGIN})                                                                 \
-  X(size, (std::pair<unsigned int, unsigned int>), {1280, 720})                                                        \
-  X(mode, ::mode, {WINDOWED})                                                                                          \
-  X(vsync, bool, {true})
-    ENLIST(window, WINDOW);
-#undef WINDOW
+    ENLIST(game,                    //
+           (master, double, {0.5}), //
+           (sound, double, {1.0}),  //
+           (music, double, {1.0}));
+    ENLIST(window,                                                       //
+           (display, SDL_DisplayID, {PRIMARY}),                          //
+           (position, (std::pair<int, int>), {ORIGIN, ORIGIN}),          //
+           (size, (std::pair<unsigned int, unsigned int>), {1280, 720}), //
+           (mode, ::mode, {WINDOWED}),                                   //
+           (vsync, bool, {true}));
 
   public:
     settings() : cse::state("CSGame/settings") {}
