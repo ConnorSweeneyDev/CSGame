6a76470d7afd1d3df367cc15e1af20992cc3ec4a
Connor Sweeney <connor.sweeney1234@gmail.com>
2026-07-10 16:38:31 +0100

    Updated for better layout.

--- program/source/layout.cpp
+++ program/source/layout.cpp
@@ -15,39 +15,39 @@
 
 namespace csg
 {
-  void game::setup(const std::shared_ptr<game> &game)
+  void game::setup(const std::shared_ptr<game> &g)
   {
-    game->set<settings>("settings")
-      .set<window>()
-      .current("main", scene::main)
-      .set<cursor>("cursor")
-      .set<text>("tick", glm::dvec2{-63.0, -46.0}, glm::dvec2{50, 10})
-      .set<text>("frame", glm::dvec2{75.0, -46.0}, glm::dvec2{50, 10});
+    g->set<settings>("settings");
+    g->set<window>();
+    g->current("main", scene::main);
+    g->set<cursor>("cursor");
+    g->set<text>("tick", glm::dvec2{-63.0, -46.0}, glm::dvec2{50, 10});
+    g->set<text>("frame", glm::dvec2{75.0, -46.0}, glm::dvec2{50, 10});
   }
 
-  void scene::main(const std::shared_ptr<scene> &scene)
+  void scene::main(const std::shared_ptr<scene> &s)
   {
-    scene->set<button>("button1", glm::dvec2{-22.0, -40.0})
-      .set<button>("button2", glm::dvec2{22.0, -40.0})
-      .set<camera>(glm::dvec3{0.0, 0.0, 80.0})
-      .set<player>("player", glm::dvec3{0.0, 0.0, 0.0})
-      .set<environment>("floor", glm::dvec3{0.0, -61.0, 0.0}, image::floor, animation::floor.main)
-      .set<environment>("shop", glm::dvec3{80.0, 24.0, -1.0}, image::shop, animation::shop.main)
-      .set<environment>("background1", glm::dvec3{0.0, 80.0, -3.0}, image::background1, animation::background1.main)
-      .set<environment>("background2", glm::dvec3{0.0, 80.0, -6.0}, image::background2, animation::background2.main)
-      .set<environment>("background3", glm::dvec3{0.0, 80.0, -9.0}, image::background3, animation::background3.main)
-      .set<sun>("sun")
-      .set<lamp>("lamp1", glm::dvec3{32.0, 3.0, 0.0})
-      .set<lamp>("lamp2", glm::dvec3{130.0, 3.0, 0.0})
-      .set<spot>("spot", glm::dvec3{-80, 20.0, -9.0});
+    s->set<button>("button1", glm::dvec2{-22.0, -40.0});
+    s->set<button>("button2", glm::dvec2{22.0, -40.0});
+    s->set<camera>(glm::dvec3{0.0, 0.0, 80.0});
+    s->set<player>("player", glm::dvec3{0.0, 0.0, 0.0});
+    s->set<environment>("floor", glm::dvec3{0.0, -61.0, 0.0}, image::floor, animation::floor.main);
+    s->set<environment>("shop", glm::dvec3{80.0, 24.0, -1.0}, image::shop, animation::shop.main);
+    s->set<environment>("background1", glm::dvec3{0.0, 80.0, -3.0}, image::background1, animation::background1.main);
+    s->set<environment>("background2", glm::dvec3{0.0, 80.0, -6.0}, image::background2, animation::background2.main);
+    s->set<environment>("background3", glm::dvec3{0.0, 80.0, -9.0}, image::background3, animation::background3.main);
+    s->set<sun>("sun");
+    s->set<lamp>("lamp1", glm::dvec3{32.0, 3.0, 0.0});
+    s->set<lamp>("lamp2", glm::dvec3{130.0, 3.0, 0.0});
+    s->set<spot>("spot", glm::dvec3{-80, 20.0, -9.0});
   }
 
-  void scene::other(const std::shared_ptr<scene> &scene)
+  void scene::other(const std::shared_ptr<scene> &s)
   {
-    scene->set<camera>(glm::dvec3{0.0, 0.0, 80.0})
-      .set<player>("player", glm::dvec3{0.0, 0.0, 0.0})
-      .set<environment>("floor", glm::dvec3{0.0, -61.0, 0.0}, image::floor, animation::floor.main)
-      .set<environment>("background3", glm::dvec3{0.0, 80.0, -9.0}, image::background3, animation::background3.main)
-      .set<sun>("sun");
+    s->set<camera>(glm::dvec3{0.0, 0.0, 80.0});
+    s->set<player>("player", glm::dvec3{0.0, 0.0, 0.0});
+    s->set<environment>("floor", glm::dvec3{0.0, -61.0, 0.0}, image::floor, animation::floor.main);
+    s->set<environment>("background3", glm::dvec3{0.0, 80.0, -9.0}, image::background3, animation::background3.main);
+    s->set<sun>("sun");
   }
 }
