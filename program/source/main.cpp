6b40c8463df57e11a490a5e80d27117f0a4b5d29
Connor Sweeney <connor.sweeney1234@gmail.com>
2026-07-10 14:01:48 +0100

    Improved main.

--- program/source/main.cpp
+++ program/source/main.cpp
@@ -7,7 +7,7 @@
 
 int cse::main(int argc, char *argv[])
 {
-  if (argc > 1 || !argv[0]) throw exception("Expected 1 argument, got {}", argc);
+  if (argc != 1 || !argv[0]) throw exception("Expected 1 argument, got {}", argc);
   game::create(csg::game::setup)->run();
   return success;
 }
