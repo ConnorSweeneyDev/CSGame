d113a6d622be938c9d9da99ea5e9d343cd81d10b
Connor Sweeney <connor.sweeney1234@gmail.com>
2026-07-18 15:43:51 +0100

    Updated for pivots.

--- program/source/layout.cpp
+++ program/source/layout.cpp
@@ -21,8 +21,8 @@ namespace csg
     g->set<window>();
     g->current("main", scene::main);
     g->set<cursor>("cursor");
-    g->set<text>("tick", glm::dvec2{-63.0, 46.0}, glm::dvec2{50, 10});
-    g->set<text>("frame", glm::dvec2{75.0, 46.0}, glm::dvec2{50, 10});
+    g->set<text>("tick", glm::dvec2{-63.0, 46.0}, glm::dvec2{50.0, 10.0});
+    g->set<text>("frame", glm::dvec2{75.0, 46.0}, glm::dvec2{50.0, 10.0});
   }
 
   void scene::main(const std::shared_ptr<scene> &s)
@@ -30,7 +30,7 @@ namespace csg
     s->set<button>("button1", glm::dvec2{-22.0, 40.0});
     s->set<button>("button2", glm::dvec2{22.0, 40.0});
     s->set<camera>(glm::dvec3{0.0, 0.0, 80.0});
-    s->set<player>("player", glm::dvec3{0.0, 0.0, 0.0});
+    s->set<player>("player", glm::dvec3{0.0, -6.0, 0.0});
     s->set<environment>("floor", glm::dvec3{0.0, -61.0, 0.0}, image::floor, animation::floor.main);
     s->set<environment>("shop", glm::dvec3{80.0, 24.0, -1.0}, image::shop, animation::shop.main);
     s->set<environment>("background1", glm::dvec3{0.0, 80.0, -3.0}, image::background1, animation::background1.main);
@@ -45,7 +45,7 @@ namespace csg
   void scene::other(const std::shared_ptr<scene> &s)
   {
     s->set<camera>(glm::dvec3{0.0, 0.0, 80.0});
-    s->set<player>("player", glm::dvec3{0.0, 0.0, 0.0});
+    s->set<player>("player", glm::dvec3{0.0, -6.0, 0.0});
     s->set<environment>("floor", glm::dvec3{0.0, -61.0, 0.0}, image::floor, animation::floor.main);
     s->set<environment>("background3", glm::dvec3{0.0, 80.0, -9.0}, image::background3, animation::background3.main);
     s->set<sun>("sun");
